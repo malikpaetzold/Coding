61e93200518fced261446cedb1331759e4bdf2ed
Patzold <60754058+Patzold@users.noreply.github.com>
2020-12-27 13:22:33 +0100

    Create linked list

--- LinkedList.py
+++ LinkedList.py
@@ -33,3 +33,49 @@ def print_linked_list(head):
 
 print_linked_list(head)
 
+# Creating a Linked List by iterating over input
+
+def create_linked_list(input_list):
+    head = None
+    tail = None
+    
+    for value in input_list:
+        if head is None:
+            head = Node(value)
+            tail = head
+        else:
+            tail.next = Node(value)
+            tail = tail.next
+    
+    return head
+
+### TEST CODE ###
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: "  + e)
+        
+        
+
+input_list = [1, 2, 3, 4, 5, 6]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = [1]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = []
+head = create_linked_list(input_list)
+test_function(input_list, head)
