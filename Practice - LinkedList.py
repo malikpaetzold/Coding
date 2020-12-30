6b55ce44ef5bb580d366380650027df6da9c322c
Patzold <60754058+Patzold@users.noreply.github.com>
2020-12-30 23:01:18 +0100

    Practice Linked Lists (Solution included)

--- Practice - LinkedList.py
+++ Practice - LinkedList.py
@@ -117,3 +117,170 @@ value = linked_list.pop()
 assert value == 2, f"list contents: {linked_list.to_list()}"
 assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
 
+def insert(self, value, pos):
+    """ Insert value at pos position in the list. If pos is larger than the
+    length of the list, append to the end of the list. """
+    if pos > 0:    
+        current_node = self.head
+        for i in range(pos - 1):
+            if current_node.next is not None:
+                current_node = current_node.next
+        continuation = current_node.next
+        current_node.next = Node(value)
+        current_node.next.next = continuation
+        return
+    else:
+        continuation = self.head
+        self.head = Node(value)
+        self.head.next = continuation
+
+LinkedList.insert = insert
+
+# Test insert 
+linked_list.insert(5, 0)
+assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(2, 1)
+assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(3, 6)
+assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+
+def size(self):
+    """ Return the size or length of the linked list. """
+    # TODO: Write function to get size here
+    length = 1
+    current_node = self.head
+    while current_node.next is not None:
+        length += 1
+        current_node = current_node.next
+    return length
+
+LinkedList.size = size
+
+# Test size function
+assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"
+
+
+#####################################################################################
+#####       SOLUTION                                        #########################
+#####################################################################################
+
+# Solution
+
+#----------------------------------------------------#
+def prepend(self, value):
+    """ Prepend a node to the beginning of the list """
+
+    if self.head is None:
+        self.head = Node(value)
+        return
+
+    new_head = Node(value)
+    new_head.next = self.head
+    self.head = new_head
+#----------------------------------------------------#
+def append(self, value):
+    """ Append a node to the end of the list """
+    # Here I'm not keeping track of the tail. It's possible to store the tail
+    # as well as the head, which makes appending like this an O(1) operation.
+    # Otherwise, it's an O(N) operation as you have to iterate through the
+    # entire list to add a new tail.
+
+    if self.head is None:
+        self.head = Node(value)
+        return
+
+    node = self.head
+    while node.next:
+        node = node.next
+
+    node.next = Node(value)
+#----------------------------------------------------#
+def search(self, value):
+    """ Search the linked list for a node with the requested value and return the node. """
+    if self.head is None:
+        return None
+
+    node = self.head
+    while node:
+        if node.value == value:
+            return node
+        node = node.next
+
+    raise ValueError("Value not found in the list.")
+
+#----------------------------------------------------#
+def remove(self, value):
+    """ Delete the first node with the desired data. """
+    if self.head is None:
+        return
+
+    if self.head.value == value:
+        self.head = self.head.next
+        return
+
+    node = self.head
+    while node.next:
+        if node.next.value == value:
+            node.next = node.next.next
+            return
+        node = node.next
+
+    raise ValueError("Value not found in the list.")
+
+#----------------------------------------------------#
+def pop(self):
+    """ Return the first node's value and remove it from the list. """
+    if self.head is None:
+        return None
+
+    node = self.head
+    self.head = self.head.next
+
+    return node.value
+#----------------------------------------------------#
+def insert(self, value, pos):
+    """ Insert value at pos position in the list. If pos is larger than the
+        length of the list, append to the end of the list. """
+    # If the list is empty 
+    if self.head is None:
+        self.head = Node(value)
+        return
+        
+    if pos == 0:
+        self.prepend(value)
+        return
+
+    index = 0
+    node = self.head
+    while node.next and index <= pos:
+        if (pos - 1) == index:
+            new_node = Node(value)
+            new_node.next = node.next
+            node.next = new_node
+            return
+
+        index += 1
+        node = node.next
+    else:
+        self.append(value)
+        
+#----------------------------------------------------#
+
+def size(self):
+    """ Return the size or length of the linked list. """
+    size = 0
+    node = self.head
+    while node:
+        size += 1
+        node = node.next
+
+    return size
+#----------------------------------------------------#
+
+def to_list(self):
+    out = []
+    node = self.head
+    while node:
+        out.append(node.value)
+        node = node.next
+    return out
--- findParityOutlier.py
+++ findParityOutlier.py
@@ -1,1 +1,6 @@
 print(1%2)
+
+l = 2
+
+for i in range(l -1):
+    print("Hello!")
