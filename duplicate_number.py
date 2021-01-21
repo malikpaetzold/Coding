fdf5f3ea6841554c5148e481e6d6dde7e535ae19
Patzold <60754058+Patzold@users.noreply.github.com>
2021-01-21 17:47:00 +0100

    Duplicate Number

---
+++ duplicate_number.py
@@ -0,0 +1,48 @@
+"""
+Problem Statement
+You have been given an array of length = n. The array contains integers from 0 to n - 2. Each number in the array is present exactly once except for one number which is present twice. Find and return this duplicate number present in the array
+
+Example:
+
+arr = [0, 2, 3, 1, 4, 5, 3]
+output = 3 (because 3 is present twice)
+
+"""
+
+def duplicate_number(arr):
+    """
+    :param - array containing numbers in the range [0, len(arr) - 2]
+    return - the number that is duplicate in the arr
+    """
+    for i in range(len(arr)):
+        temp = arr[i]
+        for j in range(i, len(arr), 1):
+            if temp == arr[j]: return temp
+    return None
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    output = duplicate_number(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr = [0, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 4, 3, 2, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 5, 3, 2, 4]
+solution = 5
+
+test_case = [arr, solution]
+test_function(test_case)
